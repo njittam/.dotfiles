9e3264aaf863dfc7591d2f3c684b0e07155ea2cd
njittam <git@mattijnkreuzen.nl>
2020-09-02 19:11:58 +0200

    added code plugins

---
+++ install_code.sh
@@ -0,0 +1,5 @@
+code --install-extension james-yu.latex-workshop
+code --install-extension golang.go
+code --install-extension ms-python.python
+code --install-extension eamodio.gitlens
+code --install-extension samghelms.jupyter-notebook-vscode
